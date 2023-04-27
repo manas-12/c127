4a182d1d285375841f2e4b7bd80e9fec15e35721
manas-12 <92636422+manas-12@users.noreply.github.com>
2023-04-27 20:24:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+peter_pan_song = "";
+Harry_Potter_Theme_song = "";
+
+function setup()
+{
+    canvas = createCanvas(600,600);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video,0,0,600,600);
+}
+
+function preload(){
+    peter_pan_song = loadSound("music2.mp3");
+    Harry_Potter_Theme_song = loadSound("music.mp3");
+}
